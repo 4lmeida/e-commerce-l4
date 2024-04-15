7d4c39d4bdcd9a9c7a2b6ac4d09358d357d3258e
Luis Almeida <lferreira646@gmail.com>
2024-04-15 17:49:40 -0300

    Project created and maven update plugin

---
+++ backend/src/main/java/com/dev/l4lmeida/l4ecommerce/L4ecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.l4lmeida.l4ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L4ecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L4ecommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/l4lmeida/l4ecommerce/L4ecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.l4lmeida.l4ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L4ecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
